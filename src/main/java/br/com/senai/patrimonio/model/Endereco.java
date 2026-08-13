c46e81db8563480d1062f2e2d486b05367e904a4
KAUÃ SCOTTI SARVALAIO <kaua_sarvalaio@estudante.sesisenai.org.br>
2026-08-13 19:42:36 -0300

    lets balatrar

---
+++ src/main/java/br/com/senai/patrimonio/model/Endereco.java
@@ -0,0 +1,14 @@
+package br.com.senai.patrimonio.model;
+
+public class Endereco {
+
+    private String rua;
+    private String numero;
+    private String compemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public Endereco(){
+    }
+}
